b0b6a56fdfc588218f17f107b3ea7090201ad38a
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-12-11 00:34:12 +0600

    Sorting functionality added for mobile device

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # uday-sai-react-table-component
+
+### Live Site: https://react-table-data-sorting.netlify.app/
+
+#### Features
++ Responsive react table component
++ Dynamically sorting data
++ Conditional rendering
++ Dynamic style smplemented
--- src/components/Table.js
+++ src/components/Table.js
@@ -11,7 +11,6 @@ const Table = () => {
     }, [])
 
     const sorting = (col, order) => {
-
         if (col === "ASC"){
             const sortedData = [...data].sort((a,b) => 
                 a[order].toLowerCase() > b[order].toLowerCase() ? 1 : -1)
@@ -44,15 +43,22 @@ const Table = () => {
                     <th>Status</th>
                 </thead>
                 <tbody>
+                    {/* It's only show on small device */}
+                    <div className='small-sorting'>
+                        <select onChange={(e) => sorting(e.target.value, "first_name")} name="firstName" id="">
+                            <option selected disabled>Unsorted</option>
+                            <option value="ASC">Sort by ASC</option>
+                            <option value="DSC">Sort by DESC</option>
+                        </select>
+                    </div>
                     {
-                        data.map((item) => <tr onClick={() => setSelected(item)} style={selected.id === item.id ? {backgroundColor: "blue", color: "white"} : null}>
+                        data.map((item) => <tr key={item.id} onClick={() => setSelected(item)} style={selected.id === item.id ? {backgroundColor: "blue", color: "white"} : null}>
                             <td data-aria-label='First Name'>{item.first_name}</td>
                             <td data-aria-label='Last Name'>{item.last_name}</td>
                             <td data-aria-label='Age'>{item.age}</td>
                             <td data-aria-label='Full Name'>{item.first_name} {item.last_name}</td>
                             <td data-aria-label='status' style={item.status ? { backgroundColor: "green", color: "white" } : { backgroundColor: "red", color: "white" } }>{item.status} {item.status ? "True" : "False" }</td>
                             </tr>
-                            
                         )
                     }
                 </tbody>
